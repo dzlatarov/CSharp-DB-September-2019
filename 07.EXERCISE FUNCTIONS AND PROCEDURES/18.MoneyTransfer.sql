bf9ae4cc996c9479bf868dce43f39494a2d3b364
dzlatarov <danzlatarov1@gmail.com>
2019-10-11 16:40:17 +0300

    Added new query

--- 07.EXERCISE FUNCTIONS AND PROCEDURES/18.MoneyTransfer.sql
+++ 07.EXERCISE FUNCTIONS AND PROCEDURES/18.MoneyTransfer.sql
@@ -1,9 +1,6 @@
 CREATE PROC usp_TransferMoney (@SenderId INT, @ReceiverId INT, @Amount DECIMAL(18,4))
 AS
-BEGIN TRANSACTION
-	DECLARE @sender INT = (SELECT a.Id FROM Accounts a WHERE a.Id = @SenderId)
-	DECLARE @receiver INT = (SELECT a.Id FROM Accounts a WHERE a.Id = @ReceiverId)
-	
+BEGIN TRANSACTION		
 	EXEC usp_DepositMoney @ReceiverId, @Amount
 	EXEC usp_WithdrawMoney @SenderId, @Amount
 COMMIT
