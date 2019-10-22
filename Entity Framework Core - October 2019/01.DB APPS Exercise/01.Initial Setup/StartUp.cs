6233a340703e78e1d765612605059776a80a6d9f
dzlatarov <danzlatarov1@gmail.com>
2019-10-22 18:30:19 +0300

    commit refactor startUp

--- Entity Framework Core - October 2019/01.DB APPS Exercise/01.Initial Setup/StartUp.cs
+++ Entity Framework Core - October 2019/01.DB APPS Exercise/01.Initial Setup/StartUp.cs
@@ -32,17 +32,17 @@
 
                 string[] insertData =
                 {
-                    "INSERT INTO Countries (Name) VALUES ('Bulgaria'), ('Germany'), ('Greece'), ('Poland'), ('Croatia')",
+                    "INSERT INTO Countries ([Name]) VALUES ('Bulgaria'),('England'),('Cyprus'),('Germany'),('Norway')",
 
-                    "INSERT INTO Towns (Name, CountryCode) VALUES ('Sofia', 1), ('Berlin', 2), ('Lefkada', 3), ('Krakow', 4), ('Zagreb', 5)",
+                    "INSERT INTO Towns ([Name], CountryCode) VALUES ('Plovdiv', 1),('Varna', 1),('Burgas', 1),('Sofia', 1),('London', 2),('Southampton', 2),('Bath', 2),('Liverpool', 2),('Berlin', 3),('Frankfurt', 3),('Oslo', 4)",
 
-                    "INSERT INTO Minions (Name, Age, TownId) VALUES ('Ivan', 34, 1), ('Frank', 25, 2), ('Niko', 40, 3), ('Irina', 18, 4), ('Antonio', 30, 5)",
+                    "INSERT INTO Minions (Name,Age, TownId) VALUES('Bob', 42, 3),('Kevin', 1, 1),('Bob ', 32, 6),('Simon', 45, 3),('Cathleen', 11, 2),('Carry ', 50, 10),('Becky', 125, 5),('Mars', 21, 1),('Misho', 5, 10),('Zoe', 125, 5),('Json', 21, 1)",
 
-                    "INSERT INTO EvilnessFactors (Name) VALUES ('super good'), ('good'), ('bad'), ('evil'), ('super evil')",
+                    "INSERT INTO EvilnessFactors (Name) VALUES ('Super good'),('Good'),('Bad'), ('Evil'),('Super evil')",
 
-                    "INSERT INTO Villains (Name, EvilnessFactorId) VALUES ('George', 1), ('Petar', 2), ('Grisho', 3), ('Lubo', 4), ('Genadi', 5)",
+                    "INSERT INTO Villains (Name, EvilnessFactorId) VALUES ('Gru',2),('Victor',1),('Jilly',3),('Miro',4),('Rosen',5),('Dimityr',1),('Dobromir',2)",
 
-                    "INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (1, 5), (2, 5), (3, 1), (3,2), (4,3), (4,2), (5,2) , (5,4)"
+                    "INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (4,2),(1,1),(5,7),(3,5),(2,6),(11,5),(8,4),(9,7),(7,1),(1,3),(7,3),(5,3),(4,3),(1,2),(2,1),(2,7)"
                 };
 
                 foreach (var data in insertData)
