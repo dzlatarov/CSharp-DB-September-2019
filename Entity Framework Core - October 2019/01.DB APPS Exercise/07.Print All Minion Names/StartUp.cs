ef8b20a0006a5318ec117abae0ecb08963bed567
dzlatarov <danzlatarov1@gmail.com>
2019-10-25 10:26:40 +0300

    Added new query with ADO.NET

---
+++ Entity Framework Core - October 2019/01.DB APPS Exercise/07.Print All Minion Names/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace PrintAllMinionNames
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using InitialSetup;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<string> minions = GetMinionNames();
+
+            for (int i = 0; i < minions.Count / 2; i++)
+            {
+                Console.WriteLine(minions[i]);
+                Console.WriteLine(minions[minions.Count - 1 - i]);
+
+                if(minions.Count % 2 != 0)
+                {
+                    Console.WriteLine(minions[minions.Count / 2]);
+                }
+            }
+        }
+
+        private static List<string> GetMinionNames()
+        {
+            List<string> names = new List<string>();
+
+            using(var connection = new SqlConnection(Connection.ConnectionString))
+            {
+                connection.Open();
+
+                string getMinionNamesQuery = @"SELECT Name FROM Minions";
+
+                using(var command = new SqlCommand(getMinionNamesQuery, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            names.Add((string)reader[0]);
+                        }
+                    }
+                }
+            }
+
+            return names;
+        }
+    }
+}
