7c73258d32ffe93dc60cc7420118dae14ef7d067
dzlatarov <danzlatarov1@gmail.com>
2019-10-29 14:39:21 +0200

    Commit employees full information

---
+++ Entity Framework Core - October 2019/03.EXERCISE ENTITYFRAMEWORK INTRODUCTION/03.EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Text;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new SoftUniContext())
+            {
+                string result = GetEmployeesFullInformation(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                 .OrderBy(em => em.EmployeeId);
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
