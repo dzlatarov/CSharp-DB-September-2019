5c13dadabac982b2a67785c1f39f0dfb4e459022
dzlatarov <danzlatarov1@gmail.com>
2019-10-29 15:07:53 +0200

    Commit employees with over salary over 50000

---
+++ Entity Framework Core - October 2019/03.EXERCISE ENTITYFRAMEWORK INTRODUCTION/04.EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Text;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new SoftUniContext())
+            {
+                string result = GetEmployeesWithSalaryOver50000(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(em => em.Salary > 50000)
+                .OrderBy(em => em.FirstName);
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                 .OrderBy(em => em.EmployeeId);
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
