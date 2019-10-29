a7e028ba5b48568eddac916e1ab72ef01fbb8802
dzlatarov <danzlatarov1@gmail.com>
2019-10-29 15:09:30 +0200

    Commit Employees from Research and Development

---
+++ Entity Framework Core - October 2019/03.EXERCISE ENTITYFRAMEWORK INTRODUCTION/05.EmployeesFromResearchAndDevelopment/StartUp.cs
@@ -0,0 +1,74 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Text;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new SoftUniContext())
+            {
+                string result = GetEmployeesFromResearchAndDevelopment(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(em => new
+                {
+                    em.FirstName,
+                    em.LastName,
+                    DepartmentName = em.Department.Name,
+                    em.Salary
+                })
+            .Where(em => em.DepartmentName == "Research and Development")
+            .OrderBy(em => em.Salary)
+            .ThenByDescending(em => em.FirstName);
+
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.DepartmentName} - ${employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(em => em.Salary > 50000)
+                .OrderBy(em => em.FirstName);
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                 .OrderBy(em => em.EmployeeId);
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
