38ce5f6eae4a007f779df07334b6d992245f41d6
dzlatarov <danzlatarov1@gmail.com>
2019-10-31 16:47:13 +0200

    Commit code first method Hospital Database exercise

---
+++ Entity Framework Core - October 2019/04.EXERCISE CODE-FIRST/P01_HospitalDatabase/Data/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace P01_HospitalDatabase.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Config
+    {
+        public const string ConfigurationString = "Server=.\\SQLEXPRESS01;Database=Hospital;Integrated Security=True";
+    }
+}
