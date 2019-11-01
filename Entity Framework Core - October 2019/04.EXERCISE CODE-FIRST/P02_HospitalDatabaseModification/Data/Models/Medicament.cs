f07144f267ffc7335336016500b54f4cd57f4b03
dzlatarov <danzlatarov1@gmail.com>
2019-11-01 09:52:19 +0200

    Commit Hospital database modification added doctor class and migrations

---
+++ Entity Framework Core - October 2019/04.EXERCISE CODE-FIRST/P02_HospitalDatabaseModification/Data/Models/Medicament.cs
@@ -0,0 +1,20 @@
+﻿namespace P01_HospitalDatabase.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Medicament
+    {
+        public Medicament()
+        {
+            this.Prescriptions = new HashSet<PatientMedicament>();
+        }
+
+        public int MedicamentId { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<PatientMedicament> Prescriptions { get; set; }
+    }
+}
